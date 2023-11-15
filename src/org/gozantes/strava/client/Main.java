76327d038adf877980c1ca39d922da6ac992ed5b
arrieniker <iker.arrien@opendeusto.es>
2023-11-15 16:31:54 +0100

    rename de las ventanas y creacion de controllers

--- src/org/gozantes/strava/client/Main.java
+++ src/org/gozantes/strava/client/Main.java
@@ -10,7 +10,7 @@ public class Main {
 
     public static void main(String[] args) {
         User user = new User("daniel.nocito@opendeusto.es", "Daniel", new Date(2003, 7, 4), 84.00, 1.87, null, null);
-        SwingUtilities.invokeLater(() -> new VentanaSecundaria(user));
+        //SwingUtilities.invokeLater(() -> new VentanaSecundaria(user));
         //SwingUtilities.invokeLater(() -> new VentanaPrincipal());
 
     }

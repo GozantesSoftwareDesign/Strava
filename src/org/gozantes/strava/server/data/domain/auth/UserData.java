7ae8ec9430165690de05717ca6cdbce304bea012
Mikel Pintado <mikelpint@protonmail.com>
2023-11-15 18:18:33 +0100

    Iker llorón.

--- src/org/gozantes/strava/server/data/domain/auth/UserData.java
+++ src/org/gozantes/strava/server/data/domain/auth/UserData.java
@@ -10,62 +10,68 @@ import java.util.Date;
 import java.util.List;
 import java.util.Objects;
 
-public record UserData(Date birth, BigDecimal weight, Integer height,
-                       Pair<Integer, Integer[]> heartRate) {
-    public UserData(Date birth, BigDecimal weight, Integer height,
-                    Integer maxHeartRate, Integer[] heartRateMeasures) {
-        this(birth, weight, height, new Pair<Integer, Integer[]>(maxHeartRate, heartRateMeasures));
-    }
+public record UserData(String name, Date birth, BigDecimal weight, Integer height,
+        Pair <Integer, Integer> heartRate) {
+    public UserData (String name, Date birth, BigDecimal weight, Integer height, Pair <Integer, Integer> heartRate) {
+        Objects.requireNonNull (name);
+        Objects.requireNonNull (birth);
 
-    public UserData(Date birth, BigDecimal weight, Integer height,
-                    Pair<Integer, Integer[]> heartRate) {
-        Objects.requireNonNull(birth);
+        if (name.isEmpty ())
+            throw new RuntimeException ("Names must not be empty strings");
 
-        Calendar cal = Calendar.getInstance();
-        cal.set(1900, 01, 01);
+        Calendar cal = Calendar.getInstance ();
+        cal.set (1900, Calendar.JANUARY, 1);
 
-        if (cal.getTime().compareTo(birth) > 0)
-            throw new DateTimeException(String.format("Users born before %s are not allowed.", new SimpleDateFormat("dd/MM/YYYY").format(cal.getTime())));
+        if (cal.getTime ().compareTo (birth) > 0)
+            throw new DateTimeException (String.format ("Users born before %s are not allowed.",
+                    new SimpleDateFormat ("dd/MM/yyyy").format (cal.getTime ())));
 
-        else if (Calendar.getInstance().getTime().compareTo(birth) < 0)
-            throw new DateTimeException("Users born after today's date are not allowed.");
+        else if (Calendar.getInstance ().getTime ().compareTo (birth) < 0)
+            throw new DateTimeException ("Users born after today's date are not allowed.");
 
-        if (weight != null && weight.compareTo(BigDecimal.ZERO) <= 0)
-            throw new RuntimeException("Weight must be a positive number.");
+        if (weight != null && weight.compareTo (BigDecimal.ZERO) <= 0)
+            throw new RuntimeException ("Weight must be a positive number.");
 
         if (height != null && height <= 0)
-            throw new RuntimeException("Height must be a positive number.");
+            throw new RuntimeException ("Height must be a positive number.");
 
         if (heartRate == null)
-            heartRate = new Pair<Integer, Integer[]>(null, null);
+            heartRate = new Pair <Integer, Integer> (null, null);
 
-        for (Integer i : List.of(heartRate.y() == null ? new Integer[0] : heartRate.y()))
-            if (i == null || i <= 0)
-                throw new RuntimeException("Heart rates must be positive.");
-
+        this.name = name;
         this.birth = birth;
         this.weight = weight;
         this.height = height;
         this.heartRate = heartRate;
     }
 
-    public UserData(UserData data) {
-        this(data.birth, data.weight, data.height, data.heartRate);
+    public UserData (UserData data) {
+        this (data.name, data.birth, data.weight, data.height, data.heartRate);
     }
 
-    public UserData(UserData data, Date birth) {
-        this(birth, data == null ? null : data.weight, data == null ? null : data.height, data == null ? null : data.heartRate);
+    public UserData (UserData data, String name) {
+        this (name, data == null ? null : data.birth, data == null ? null : data.weight,
+                data == null ? null : data.height, data == null ? null : data.heartRate);
     }
 
-    public UserData(UserData data, BigDecimal weight) {
-        this(data.birth, weight, data.height, data.heartRate);
+    public UserData (UserData data, Date birth) {
+        this (data == null ? null : data.name, birth, data == null ? null : data.weight,
+                data == null ? null : data.height, data == null ? null : data.heartRate);
     }
 
-    public UserData(UserData data, Integer height) {
-        this(data.birth, data.weight, height, data.heartRate);
+    public UserData (UserData data, BigDecimal weight) {
+        this (data == null ? null : data.name, data == null ? null : data.birth, weight,
+                data == null ? null : data.height, data == null ? null : data.heartRate);
     }
 
-    public UserData(UserData data, Pair<Integer, Integer[]> heartRate) {
-        this(data.birth, data.weight, data.height, heartRate);
+    public UserData (UserData data, Integer height) {
+        this (data == null ? null : data.name, data == null ? null : data.birth, data == null ? null : data.weight,
+                height, data == null ? null : data.heartRate);
+
+    }
+
+    public UserData (UserData data, Pair <Integer, Integer> heartRate) {
+        this (data == null ? null : data.name, data == null ? null : data.birth, data == null ? null : data.weight,
+                data == null ? null : data.height, heartRate);
     }
 }
